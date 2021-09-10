5287e06df52bb4185d34e77e7e149624d3747bcf
Kriska939 <cristinacorrado93@gmail.com>
2021-09-10 13:47:34 +0200

    added card component + images + run npm

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4>Titolo</h4>
+    <h4>Titolo Originale</h4>
+    <p>Lingua</p>
+    <p>Voto</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
