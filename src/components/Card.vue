fa95b47970ae58e32e80cdb3785fca442f16a5f9
Kriska939 <cristinacorrado93@gmail.com>
2021-09-12 13:50:38 +0200

    added language flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,18 @@
   <div>
     <h4>Titolo: {{ movie.title }}</h4>
     <h4>Titolo originale: {{ movie.original_title }}</h4>
-    <p>Lingua:{{ movie.original_language }}</p>
+    <p>
+      Lingua:
+      <span v-if="selectFlag(movie.original_language)">
+        <img
+          class="img-resize"
+          :src="selectFlag(movie.original_language)"
+          :alt="movie.original_language"
+      /></span>
+      <span v-else>
+        {{ movie.original_language }}
+      </span>
+    </p>
     <p>Rating:{{ movie.vote_average }}</p>
   </div>
 </template>
@@ -11,6 +22,18 @@
 export default {
   name: "Card",
   props: ["movie"],
+
+  methods: {
+    selectFlag(lang) {
+      if (lang === "en") {
+        return require("../assets/images/en.png");
+      } else if (lang === "it") {
+        return require("../assets/images/it.png");
+      } else {
+        return;
+      }
+    },
+  },
 };
 </script>
 
@@ -18,5 +41,10 @@ export default {
 div {
   background-color: lightblue;
   margin: 0 20px;
+}
+
+.img-resize {
+  max-width: 50px;
+  max-height: 40px;
 }
 </style>
