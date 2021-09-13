927eda6b7bc3a007a8fc41aefe458fecd642b767
Kriska939 <cristinacorrado93@gmail.com>
2021-09-13 17:01:34 +0200

    added card images

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <h4>Titolo: {{ item.title || item.name }}</h4>
-    <h4>Titolo originale: {{ item.original_title || item.original_name }}</h4>
-    <p>
-      Lingua:
-      <span v-if="selectFlag(item.original_language)">
-        <img
-          class="img-resize"
-          :src="selectFlag(item.original_language)"
-          :alt="item.original_language"
-      /></span>
-      <span v-else>
-        {{ item.original_language }}
-      </span>
-    </p>
-    <p>Rating:{{ item.vote_average }}</p>
+    <div class="poster">
+      <img :src="getPoster(item.poster_path)" alt="Poster" />
+    </div>
+    <div class="content">
+      <h4>Titolo: {{ item.title || item.name }}</h4>
+      <h4>Titolo originale: {{ item.original_title || item.original_name }}</h4>
+      <p>
+        Lingua:
+        <span v-if="selectFlag(item.original_language)">
+          <img
+            class="img-resize"
+            :src="selectFlag(item.original_language)"
+            :alt="item.original_language"
+        /></span>
+        <span v-else>
+          {{ item.original_language }}
+        </span>
+      </p>
+      <p>Rating:{{ item.vote_average }}</p>
+    </div>
   </div>
 </template>
 
@@ -31,6 +36,14 @@ export default {
         return require("../assets/images/it.png");
       } else {
         return;
+      }
+    },
+
+    getPoster(address) {
+      if (address) {
+        return `https://image.tmdb.org/t/p/w342${address}`;
+      } else {
+        return `https://www.altavod.com/assets/images/poster-placeholder.png`;
       }
     },
   },
